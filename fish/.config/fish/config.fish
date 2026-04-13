3a3ab9715d53dbcde59a7446211fc108e6ce270b
Abdulrahman Ajlouni <ajlouni2000@gmail.com>
2026-04-14 01:52:20 +0300

    remove docker aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,13 +13,6 @@ alias open xdg-open
 alias vim nvim
 alias zed zeditor
 
-alias docker-disable "sudo systemctl disable docker.service docker.socket; echo \"🚫 Docker disabled at boot\""
-alias docker-enable "sudo systemctl enable docker.service docker.socket; echo \"✅ Docker enabled at boot\""
-alias docker-down "sudo systemctl stop docker.service docker.socket; echo \"🛑 Docker stopped\""
-alias docker-restart "sudo systemctl restart docker.service; echo \"🔁 Docker restarted\""
-alias docker-up "sudo systemctl start docker.service docker.socket; echo \"🐳 Docker started\""
-alias docker-status "systemctl status docker.service --no-pager"
-
 alias postgres-up "docker run -d --restart unless-stopped -p 127.0.0.1:5432:5432 --name=postgres18 -e POSTGRES_HOST_AUTH_METHOD=trust -v postgres18-data:/var/lib/postgresql postgres:18"
 alias psql-local "psql -h localhost -U postgres"
 alias redis-up "docker run -d --restart unless-stopped -p 127.0.0.1:6379:6379 --name=redis8 redis:8"
