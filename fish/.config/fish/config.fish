f570e98ef6ab22374867baaf6c78d8cb11995fdd
Abdulrahman Ajlouni <ajlouni2000@gmail.com>
2026-05-05 04:03:07 +0300

    switch to omarchy-fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,3 @@
-# Disable welcome message
-function fish_greeting; end
-
-fish_add_path $HOME/.local/bin
-
-zoxide init fish --cmd cd | source
-
 # Set personal aliases
 alias lzd lazydocker
 alias lzg lazygit
@@ -12,7 +5,6 @@ alias oc opencode
 alias open xdg-open
 alias vim nvim
 alias zed zeditor
-
 alias postgres-up "docker run -d --restart unless-stopped -p 127.0.0.1:5432:5432 --name=postgres18 -e POSTGRES_HOST_AUTH_METHOD=trust -v postgres18-data:/var/lib/postgresql postgres:18"
 alias psql-local "psql -h localhost -U postgres"
 alias redis-up "docker run -d --restart unless-stopped -p 127.0.0.1:6379:6379 --name=redis8 redis:8"
@@ -38,13 +30,6 @@ function zw
     __edit_cmd $argv[1] zed
 end
 
-alias ls "eza -lh --group-directories-first --icons=auto"
-
 function mcd
     mkdir -p $argv[1]; and cd $argv[1]
 end
-
-set -gx EDITOR nvim
-
-# Starship prompt
-starship init fish | source
